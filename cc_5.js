fee813d870e01152673944881b16cc604d22262e
Angel75Git <aduboncenteno@usf.edu>
2025-02-07 15:41:46 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,13 @@
+//Task 1
+//Creating a customer object
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+
+
+
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Eamil: ${customer.email}`);
