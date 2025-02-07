9095051586bc98070d6b501fc74cadd72420683c
Angel75Git <aduboncenteno@usf.edu>
2025-02-07 15:56:34 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -7,7 +7,21 @@ let customer = {
 };
 
 
-
+//Logging the properties
 console.log(`Name: ${customer.name}`);
 console.log(`Age: ${customer.age}`);
 console.log(`Eamil: ${customer.email}`);
+
+//Task 2
+let order = {
+    orderID: 12345,
+    totalAmount: 150, 
+    status: "Processing", 
+    displayOrder: function() {
+        console.log(`OrderID: ${this.orderID}`);
+        console.log(`Total: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+}
+//Calling the method
+order.displayOrder()
